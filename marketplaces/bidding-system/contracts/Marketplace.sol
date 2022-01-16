8cc34d88135e03128449b5ebbfbe632a60812f61
Zakarya Chami <zakaryachami@Zakaryas-MacBook-Pro.local>
2022-01-16 16:20:33 +0100

    update bidding system

--- marketplaces/bidding-system/contracts/Marketplace.sol
+++ marketplaces/bidding-system/contracts/Marketplace.sol
@@ -1,6 +1,151 @@
 pragma solidity ^0.8.0;
 
 import "./ERC20.sol";
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+
 // import "./NFTCollection.sol";
 // import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 // import "hardhat/console.sol"; //For debugging only
@@ -195,11 +340,18 @@ contract NFTCollection {
 
 
 contract Marketplace is IERC721Receiver {
+    using SafeMath for uint;
     // Name of the marketplace
     string public name;
 
     // Index of auctions
     uint256 public index = 0;
+
+    uint256 public commission;
+
+    address public feeAddress;
+
+    address public owner;
 
     // Structure to define auction properties
     struct Auction {
@@ -248,6 +400,15 @@ contract Marketplace is IERC721Receiver {
     // constructor of the contract
     constructor(string memory _name) {
         name = _name;
+        owner = msg.sender;
+    }
+
+      /**
+   * @dev Throws if called by any account other than the owner.
+   */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
 
     /**
@@ -261,6 +422,14 @@ contract Marketplace is IERC721Receiver {
             size := extcodesize(_addr)
         }
         return size > 0;
+    }
+
+    function setCommission(uint _commission) public onlyOwner {
+        commission = _commission;
+    }
+
+    function setOwner(address _owner) public onlyOwner {
+        owner = _owner;
     }
 
     /**
@@ -495,9 +664,15 @@ contract Marketplace is IERC721Receiver {
         ERC20 paymentToken = ERC20(auction.addressPaymentToken);
         // Transfer locked token from the marketplace
         // contract to the auction creator address
-        require(
-            paymentToken.transfer(auction.creator, auction.currentBidPrice)
-        );
+        // require(
+        //     paymentToken.transfer(auction.creator, auction.currentBidPrice)
+        // );
+
+        uint fee = ((auction.currentBidPrice).mul(commission)).div(10000);
+        uint amountAfterFee = (auction.currentBidPrice).sub(fee);
+        // paymentToken.transfer(auction.creator, auction.currentBidPrice);
+        require(paymentToken.transfer(auction.creator, amountAfterFee));
+        require(paymentToken.transfer(owner, fee));
 
         emit NFTClaimed(_auctionIndex, auction.nftId, msg.sender);
     }
@@ -540,7 +715,12 @@ contract Marketplace is IERC721Receiver {
         ERC20 paymentToken = ERC20(auction.addressPaymentToken);
         // Transfer locked tokens from the market place contract
         // to the wallet of the creator of the auction
-        paymentToken.transfer(auction.creator, auction.currentBidPrice);
+
+        uint fee = ((auction.currentBidPrice).mul(commission)).div(10000);
+        uint amountAfterFee = (auction.currentBidPrice).sub(fee);
+        // paymentToken.transfer(auction.creator, auction.currentBidPrice);
+        paymentToken.transfer(auction.creator, amountAfterFee);
+        paymentToken.transfer(owner, fee);
 
         emit TokensClaimed(_auctionIndex, auction.nftId, msg.sender);
     }
